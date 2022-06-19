dfda86d0978b01157c5086058db27d6c66147379
9382 <omnomaidan@gmail.com>
2022-06-19 18:36:32 +0100

    Change FixArchiveLocation to a stabler regex system
    Just how much more stable? Who knows, lets find out

--- Tasks/FixArchiveLocations.py
+++ Tasks/FixArchiveLocations.py
@@ -1,4 +1,3 @@
-commoncases = [r"/Archive %(counter)d", r"/Archives/%(year)d/%(monthname)s", r"/Archives/%(year)d/%(monthname)d"]
 def CheckArchiveLocations(page):
     article = Article(page)
     if not article.exists():
@@ -17,23 +16,29 @@ def CheckArchiveLocations(page):
                 #Note that this could easily fall flat when presented with GIGO as its a bit of a shot in the dark
                 if not archiveLocation.startswith(currentLocation+"/"): #Not a subpage
                     #Most common case: Result of a page move, no GIGO problems
-                    easyFix = False
-                    for commoncase in commoncases:
-                        if archiveLocation.find(commoncase) > -1:
-                            template.ChangeKeyData("archive",currentLocation+commoncase)
-                            content = content.replace(template.Original,template.Text)
-                            easyFix = True
-                            break
-                    if easyFix:
+                    existingArchive = regex.compile("(/|^)([Aa][Rr][Cc][Hh][Ii][Vv][Ee]([Ss]/| ).+)").search(archiveLocation)
+                    if existingArchive:
+                        wantedLocation = existingArchive.group()
+                        if wantedLocation[0] == "/": #Stupid but eh
+                            template.ChangeKeyData("archive",currentLocation+wantedLocation)
+                        else:
+                            template.ChangeKeyData("archive",currentLocation+"/"+wantedLocation)
+                        content = content.replace(template.Original,template.Text)
                         continue
                     #else: cry(). its GIGO time
-                    #Now we try to find any form of archive location at the end - maybe they use a custom format
-                    #A potential call to the move or edit history and some quick checks may help rare cases, but may also be excessive.
+                    #If the above check fails, vandalism is a likely scenario
+                    #While we could code something to check previous revisions or look for naming patterns, we could also leave it to humans
+                    #And thats what we shall do
+
+                    #After a recent change to the above code, this bottom code may not even run. It is being left in for now "just in case", but it can be safely ignored
+                    log(f"Somehow reached the panic GIGO step in FixArchiveLocations for {page}. This is probably worth looking into")
+
                     splitSections = archiveLocation.split("/")
                     latestOccurance = -1
                     index = 0
                     for section in splitSections:
-                        if section.lower().find("archive") > -1: #Presumably, the archives start here
+                        if section.lower().find("archive") > -1 and latestOccurance <= 0: #Presumably, the archives start here
+                            # The <= 0 check is for the chance that some page, for some reason, is titled Archive or similar
                             latestOccurance = index #Find the last one. Who knows, maybe the base page name has archive
                         index += 1
                     if latestOccurance == -1: #No archive subpage format found, defaulting to Archive %(counter)d
@@ -47,10 +52,10 @@ def CheckArchiveLocations(page):
                         template.ChangeKeyData("archive",currentLocation+"/"+"/".join(splitSections[latestOccurance:]))
                     content = content.replace(template.Original,template.Text)
                 elif content == article.RawContent: #The earlier regex.sub caught nothing and theres no archive fail - this shouldnt happen
-                    log("[FixArchiveLocation] "+page+" does not seem to be malformed. Unsure how they ended up here. Trying a null edit...")
+                    log(f"[FixArchiveLocation] {page} does not seem to be malformed. Unsure how they ended up here. Trying a null edit...")
                     content = content + "\n"
     if content != article.RawContent:
-        article.edit(content,f"Fix archive location for Lowercase Sigmabot III ([[User:MiszaBot/config#Parameters explained|More info]] - [[User talk:{username}|Report issues]])")
+        article.edit(content,f"Fix archive location for Lowercase Sigmabot III ([[User:MiszaBot/config#Parameters explained|More info]] - [[User talk:{username}|Report bot issues]])")
     return True
 
 # CheckArchiveLocations("User_talk:Aidan9382-Bot/sandbox")
