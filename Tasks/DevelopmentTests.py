a9e5d832d73a204d51296f4accc91db4acfc9310
9382 <omnomaidan@gmail.com>
2023-01-23 07:34:37 +0000

    Re-code internals to use the real API
    Also includes a bunch of basic refactoring and just making things nicer and simpler
    
    The fact that this hadn't been failing more than it already did when it comes to encoding is a shock - I was using index.php and re-creating it like I was a browser instead of spending 20 minutes to read some API documentation - plus the html regex parsing and constantly quoting/unquoting? I'm shocked this ran in the first place, but its better now
    
    Note that the FAL task still needs converting (which I will do)

--- Tasks/DevelopmentTests.py
+++ Tasks/DevelopmentTests.py
@@ -1,33 +1,37 @@
 #Demonstration and testing stuff
 
-# basename = "J'Adore Parfum d'Eau"
+# test = Article(f"User:{username}")
+# print(test,test.exists,test.Namespace,test.Title)
 
-# page = Article(f"Talk:{basename}")
-# print("Got page",page)
-# print("Linked, Linked.Linked =",page.GetLinkedPage(),page.GetLinkedPage().GetLinkedPage())
+# test = Article(f"User:{username}/–+-'\"&?")
+# print(test,test.exists,test.Namespace,test.Title)
 
-# print(page.GetSubpages())
+# print("GET CONTENT AND EDIT")
+# content = test.GetContent()
+# test.edit(content+"\nTest edit","Testing")
 
-dummy = f"User:{username}/dummy'\"%26%3Fpage"
-title = fr"User:{username}/title'\"26%3Ftest"
+# print("GET HISTORY")
+# for x in test.GetHistory(6):
+#     # print(x.Comment)
+#     print(x.IsMove())
+
+# print("GET SUBPAGES")
+# print(test.GetSubpages())
+
+# print("Move page?")
+# test.MoveTo(f"User:{username}/–+- A '\"&?","Page move test")
+
+title = f"User:{username}/–+-'\"&?"
 
 def MainTester(page):
-    article = Article(page)
-    print(page,article,article.exists())
-    print(article.GetSubpages())
+    print(page,page.exists)
+    print(page.GetSubpages(),page.GetLinkedPage())
 
-    if page == title:
+    if page.Title == title:
         print("title!")
-        for h in article.GetHistory():
-            a,b,c = h.IsMove()
-            if a:
-                print(b,c)
-                d,e = Article(b), Article(c)
-                print(d,d.exists(), "  |  ", e, e.exists())
-    elif page == dummy:
-        print("dummy!")
-        # article.edit("SA test for making sure processes work under new system","Edit test")
+        print(page)
+        print("Content:",page.GetContent())
 
-print("GOING") #testing
-IterateCategory("Category:Pages where archive parameter is not a subpage",MainTester)
+print("GOING")
+IterateCategoryPages("Category:Pages where archive parameter is not a subpage",MainTester)
 print("AND HES GONE")
