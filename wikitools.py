6a8915dbafaa60c0e9a1a9437e71b0d232b752c0
9382 <omnomaidan@gmail.com>
2023-08-11 08:15:47 +0100

    a bit extra

--- wikitools.py
+++ wikitools.py
@@ -153,7 +153,8 @@ def SetStopped(state):
     activelyStopped = state
 
 namespaces = ["User", "Wikipedia", "File", "MediaWiki", "Template", "Help", "Category", "Portal", "Draft", "TimedText", "Module"] #Gadget( definition) is deprecated
-pseudoNamespaces = {"WP":"Wikipedia", "WT":"Wikipedia talk", "Project":"Wikipedia", "Project talk":"Wikipedia talk", "Image":"File", "Image talk":"File talk"} #Special cases that dont match normal sets
+pseudoNamespaces = {"WP":"Wikipedia", "WT":"Wikipedia talk", "Project":"Wikipedia", "Project talk":"Wikipedia talk",
+                    "Image":"File", "Image talk":"File talk", "Mediawiki":"MediaWiki", "Mediawiki talk":"MediaWiki talk"} #Special cases that dont match normal sets
 namespaceIDs = {"Article":0, "Talk":1, "User":2, "User talk":3, "Wikipedia":4, "Wikipedia talk":5, "File":6, "File talk":7,
                 "MediaWiki":8, "MediaWiki talk":9, "Template":10, "Template talk":11, "Help":12, "Help talk":13,
                 "Category":14, "Category talk":15, "Portal":100, "Portal talk":101, "Draft":118, "Draft talk":119,
